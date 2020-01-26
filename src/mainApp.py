cbe7b3e794abdfd5ceb0f33b28b348295e8e968c
kevincheung00 <kevin.cheung.sh@gmail.com>
2020-01-26 04:21:34 -0500

    Update HTML files

--- src/mainApp.py
+++ src/mainApp.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template, request
 from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
 
+ALLOWED_EXTENSIONS = set(['txt', 'jpg'])
+
 app = Flask(__name__)
 
-photos = UploadSet('photos', Images)
+# photos = UploadSet('photos', Images)
+#
+# @app.route('/upload', methods=['GET', 'POST'])
+#
+# def upload():
+#     if request.method == 'POST' and 'photo' in request.files:
+#         filename = photos.save(request.files['photo'])
+#         return filename
+#
+#     return render_template('upload.html')
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
-@app.route('/upload', methods=['GET', 'POST'])
 
-def upload():
-    if request.method == 'POST' and 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
-        return filename
-
-    return render_template('upload.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/")
+def index():
+    return render_template("abc.html")
