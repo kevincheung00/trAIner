b33dc9531e9b60c3b4951273d44000c0a483af54
kevincheung00 <kevin.cheung.sh@gmail.com>
2020-01-25 23:31:24 -0500

    Added eye tracking functionality

---
+++ src/eye-detection/eyedetect.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+file = open("eyestats.txt", "w")
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
+
+eyecount = 0
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y-int((y + h) * 0.7), x:x + w]
+        roi_color = img[y:y-int((y + h) * 0.7), x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+
+
+        if type(eyes) == np.ndarray:
+            eyecount+=1
+
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ew+ew, ey+eh), (0,255,0), 2)
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+
+    if k== 27:
+        break
+
+print(eyecount)
+
+
+cap.release()
